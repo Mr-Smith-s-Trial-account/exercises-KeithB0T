099fd6f49c17e3c424d48795c0b84d3fc43c03b5
KeithB0T <50924657+KeithB0T@users.noreply.github.com>
2022-03-28 19:05:44 -0400

    Update student.hpp

--- student.hpp
+++ student.hpp
@@ -9,15 +9,33 @@ using namespace std;
 
 int exerciseOne(string word) //takes in one word
 {
-	return 15;// TODO: fix
+	if (word == "agent")
+	{
+		return 10;
+	}
+	
+	return 0;// TODO: fix
 }
 
 string exerciseTwo(int mark) //takes in one mark
 {
-	return "blarg";// TODO: fix. Returns a word
+	if (mark >= 50)
+	{
+		return "passed";
+	}
+	
+	return "failed";// TODO: fix. Returns a word
 }
 
 string exerciseThree(int number) //takes in one number
 {
+	string word = "";
+	if (number % 3 == 0){
+		word = word+"fizz";}
+	if (number%5 ==0){
+		word = word+"buzz";}
+	if (number%3 != 0 && number%5!=0){
+		word = to_string(number);}
+	return word;
 	return "0";// TODO: fix. Returns a word, or a number in string format
 }
